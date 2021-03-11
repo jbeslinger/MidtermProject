4bdd41c570b203e0384e0c7b48a7561de0e9da90
Josh Eslinger <jbeslinger@gmail.com>
2021-03-10 21:01:47 -0600

    Changed how the instructions were set up. Also added a PerformInstructions().

--- MidtermProject.cpp
+++ MidtermProject.cpp
@@ -9,6 +9,105 @@
 using namespace std;
 
 
+#pragma region CONSTANTS
+unsigned char   A       =   0           ;
+unsigned char   X       =   1           ;
+unsigned char   PC      =   2           ;
+unsigned char   SP      =   3           ;
+
+#pragma region INSTRUCTION SPECIFIERS
+unsigned char   STOP    =   0b00000000  ;
+unsigned char   BINV    =   0b00011000  ;
+unsigned char   SHFL    =   0b00011100  ;
+unsigned char   SHFR    =   0b00011110  ;
+unsigned char   ROTL    =   0b00100000  ;
+unsigned char   ROTR    =   0b00100010  ;
+unsigned char   DECI    =   0b00110000  ;
+unsigned char   DECO    =   0b00111000  ;
+unsigned char   CHRI    =   0b01001000  ;
+unsigned char   CHRO    =   0b01010000  ;
+unsigned char   ADD     =   0b01110000  ;
+unsigned char   SUB     =   0b10000000  ;
+unsigned char   AND     =   0b10010000  ;
+unsigned char   OR      =   0b10100000  ;
+unsigned char   LDR     =   0b11000000  ;
+unsigned char   LDB     =   0b11010000  ;
+unsigned char   STR     =   0b11100000  ;
+unsigned char   STB     =   0b11110000  ;
+#pragma endregion
+
+#pragma region ADDRESSING MODES
+unsigned char   IMD     =   0b000       ; // One's digit also stands for 'Immediate'
+unsigned char   DIR     =   0b001       ; // One's digit also stands for 'Indexed'
+unsigned char   INDR    =   0b010       ;
+unsigned char   SRL     =   0b011       ;
+unsigned char   SRLD    =   0b100       ;
+unsigned char   INDX    =   0b101       ;
+unsigned char   SNX     =   0b110       ;
+unsigned char   SNXD    =   0b111       ;
+#pragma endregion
+#pragma endregion
+
+#pragma region STRUCTURES
+struct OpHalf
+{
+    unsigned short int      l;
+    unsigned short int      r;
+};
+
+union Operand
+{
+    OpHalf                  h;
+    unsigned int            w;
+};
+
+struct Tern // 0000 0 000
+{
+    unsigned char c : 3;
+    unsigned char b : 1;
+    unsigned char a : 4;
+};
+struct Bin1 // 00000 000
+{
+    unsigned char b : 3;
+    unsigned char a : 5;
+};
+struct Bin2 // 0000000 0
+{
+    unsigned char b : 1;
+    unsigned char a : 7;
+};
+
+union UnaryInstruction
+{
+    Tern                    t;
+    Bin1                    b1;
+    Bin2                    b2;
+    unsigned char           w;
+};
+
+struct BinaryInstruction
+{
+    UnaryInstruction        n;
+    Operand                 op;
+};
+
+struct Registers
+{
+    unsigned short int      A_X_PC_SP[4];
+    unsigned long           IR : 24;
+};
+
+struct CPU
+{
+    Registers reg;
+};
+#pragma endregion
+
+#pragma region INSTRUCTIONS
+
+#pragma endregion
+
 #pragma region FUNCTIONS
 int HexToDec(string hex)
 {
@@ -137,6 +236,11 @@ string IsValidHex(string in, int length)
     }
 }
 
+void PerformInstruction(string code)
+{
+    
+}
+
 string WriteProgram()
 {
     const char* GREETING = "Welcome to the PEP\\8 virtual computer!\n"
@@ -175,101 +279,10 @@ string WriteProgram()
 
 void ExecuteProgram(string code)
 {
+    cout << "--PROGRAM--\n";
+
 
 }
-#pragma endregion
-
-#pragma region CONSTANTS
-unsigned char ACC_REG  =   0          ;
-unsigned char NDX_REG  =   1          ;
-
-#pragma region INSTRUCTION SPECIFIERS
-unsigned char   STOP   =   0b00000000 ;
-unsigned char   BINV   =   0b0001100  ;
-unsigned char   SHFL   =   0b0001110  ;
-unsigned char   SHFR   =   0b0001111  ;
-unsigned char   ROTL   =   0b0010000  ;
-unsigned char   ROTR   =   0b0010001  ;
-unsigned char   DECI   =   0b00110    ;
-unsigned char   DECO   =   0b00111    ;
-unsigned char   CHRI   =   0b01001    ;
-unsigned char   CHRO   =   0b01010    ;
-unsigned char   ADD    =   0b0111     ;
-unsigned char   SUB    =   0b1000     ;
-unsigned char   AND    =   0b1001     ;
-unsigned char   OR     =   0b1010     ;
-unsigned char   LDR    =   0b1100     ;
-unsigned char   LDB    =   0b1101     ;
-unsigned char   STR    =   0b1110     ;
-unsigned char   STB    =   0b1111     ;
-#pragma endregion
-
-#pragma region ADDRESSING MODES
-unsigned char   IMD    =   0b000      ; // One's digit is also Immediate
-unsigned char   DIR    =   0b001      ; // One's digit is also Indexed
-unsigned char   INDR   =   0b010      ;
-unsigned char   SRL    =   0b011      ;
-unsigned char   SRLD   =   0b100      ;
-unsigned char   INDX   =   0b101      ;
-unsigned char   SNX    =   0b110      ;
-unsigned char   SNXD   =   0b111      ;
-#pragma endregion
-#pragma endregion
-
-#pragma region STRUCTURES
-struct OpHalf
-{
-    unsigned short int      l;
-    unsigned short int      r;
-};
-
-union Operand
-{
-    OpHalf                  h;
-    unsigned int            w;
-};
-
-struct Tern // 0000 0 000
-{
-    unsigned char c     :   3;
-    unsigned char b     :   1;
-    unsigned char a     :   4;
-};
-struct Bin1 // 00000 000
-{
-    unsigned char b     :   3;
-    unsigned char a     :   5;
-};
-struct Bin2 // 0000000 0
-{
-    unsigned char b     :   1;
-    unsigned char a     :   7;
-};
-
-union UnaryInstruction
-{
-    Tern                    t;
-    Bin1                    b1;
-    Bin2                    b2;
-    unsigned char           w;
-};
-
-struct BinaryInstruction
-{
-    UnaryInstruction        n;
-    Operand                 op;
-};
-
-struct Registers
-{
-    unsigned short int      A_X_PC_SP[4];
-    unsigned long           IR : 24;
-};
-
-struct CPU
-{
-    Registers reg;
-};
 #pragma endregion
 
 #pragma region FIELDS
@@ -282,7 +295,7 @@ string          program     ;
 
 int main()
 {
-    ofstream out("program.txt");
-    out << ReadOutHex(WriteProgram());
-    out.close();
+    /*program = WriteProgram();
+    cout << endl;
+    ExecuteProgram(program);*/
 }

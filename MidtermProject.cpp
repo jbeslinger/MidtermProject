edadd45cac54991824aa279fbd3c04c70e93fc73
Josh Eslinger <jbeslinger@gmail.com>
2021-03-10 21:07:31 -0600

    Am dumb and put the instruction specifiers back to the good format.

--- MidtermProject.cpp
+++ MidtermProject.cpp
@@ -17,23 +17,23 @@ unsigned char   SP      =   3           ;
 
 #pragma region INSTRUCTION SPECIFIERS
 unsigned char   STOP    =   0b00000000  ;
-unsigned char   BINV    =   0b00011000  ;
-unsigned char   SHFL    =   0b00011100  ;
-unsigned char   SHFR    =   0b00011110  ;
-unsigned char   ROTL    =   0b00100000  ;
-unsigned char   ROTR    =   0b00100010  ;
-unsigned char   DECI    =   0b00110000  ;
-unsigned char   DECO    =   0b00111000  ;
-unsigned char   CHRI    =   0b01001000  ;
-unsigned char   CHRO    =   0b01010000  ;
-unsigned char   ADD     =   0b01110000  ;
-unsigned char   SUB     =   0b10000000  ;
-unsigned char   AND     =   0b10010000  ;
-unsigned char   OR      =   0b10100000  ;
-unsigned char   LDR     =   0b11000000  ;
-unsigned char   LDB     =   0b11010000  ;
-unsigned char   STR     =   0b11100000  ;
-unsigned char   STB     =   0b11110000  ;
+unsigned char   BINV    =   0b0001100   ;
+unsigned char   SHFL    =   0b0001110   ;
+unsigned char   SHFR    =   0b0001111   ;
+unsigned char   ROTL    =   0b0010000   ;
+unsigned char   ROTR    =   0b0010001   ;
+unsigned char   DECI    =   0b00110     ;
+unsigned char   DECO    =   0b00111     ;
+unsigned char   CHRI    =   0b01001     ;
+unsigned char   CHRO    =   0b01010     ;
+unsigned char   ADD     =   0b0111      ;
+unsigned char   SUB     =   0b1000      ;
+unsigned char   AND     =   0b1001      ;
+unsigned char   OR      =   0b1010      ;
+unsigned char   LDR     =   0b1100      ;
+unsigned char   LDB     =   0b1101      ;
+unsigned char   STR     =   0b1110      ;
+unsigned char   STB     =   0b1111      ;
 #pragma endregion
 
 #pragma region ADDRESSING MODES
@@ -236,7 +236,7 @@ string IsValidHex(string in, int length)
     }
 }
 
-void PerformInstruction(string code)
+void ExecuteInstruction(string code)
 {
     
 }
